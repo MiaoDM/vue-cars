aef561d3ce76b9160dd1ede0ee232b21a0e31b02
erguoqie <1643317686@qq.com>
2020-10-24 18:43:45 +0800

    add amap

---
+++ src/views/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="amap-wrap">
+    <el-amap
+        vid="amapDemo"
+        class="amap-demo"
+        :zoom="zoom"
+        :center="center"
+    >
+    </el-amap>
+  </div>
+</template>
+<script>
+export default {
+  name: "index",
+  data(){
+    return{
+      zoom:15,
+      center:[116.397451,39.909187]
+    }
+  }
+}
+</script>
+<style lang="scss">
+.amap-wrap{
+  height: 100vh;
+}
+</style>
